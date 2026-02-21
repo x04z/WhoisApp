f5de99e98d030c6b7e5cbfae7f9199d3fed41327
m83 <nukan5446@gmail.com>
2026-02-21 16:47:24 +0900

    Update WhoisApp.py

--- WhoisApp.py
+++ WhoisApp.py
@@ -24,7 +24,7 @@ from openpyxl.chart.layout import Layout, ManualLayout
 from openpyxl.styles import Font, PatternFill, Alignment, Border, Side
 
 # ページ設定
-st.set_page_config(layout="wide", page_title="検索大臣", page_icon="🔎")
+st.set_page_config(layout="wide", page_title="検索大臣", page_icon="img/logo.png")
 
 # ==========================================
 # ⚙️ [Local User Config] API Key Hardcoding
